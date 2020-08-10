79a7b885367fb9c4564c8e75380175cee6446423
Kasra Jannati <kasra@redngen.com>
2020-08-10 17:42:06 -0400

    update

--- test/metacoin.js
+++ test/metacoin.js
@@ -38,3 +38,6 @@ contract('MetaCoin', (accounts) => {
     assert.equal(accountTwoEndingBalance, accountTwoStartingBalance + amount, "Amount wasn't correctly sent to the receiver");
   });
 });
+
+
+
