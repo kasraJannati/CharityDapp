a4508344ec08e1c8da39d51dbcde9644a77f2e18
Kasra Jannati <kasra_ja1991@yahoo.com>
2020-08-13 10:39:05 -0400

    update

--- truffle-config.js
+++ truffle-config.js
@@ -7,13 +7,13 @@ module.exports = {
   //
   networks: {
    development: {
-    //  host: "127.0.0.1",
-    //  port: 7545,
-    //  network_id: "*"
+     host: "127.0.0.1",
+     port: 7545,
+     network_id: "*"
 
-     host: "localhost",
-     port: 8545,
-     network_id: "*" 
+    //  host: "localhost",
+    //  port: 8545,
+    //  network_id: "*" 
    }
   //  test: {
   //    host: "127.0.0.1",
